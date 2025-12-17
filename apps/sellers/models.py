ba2c02ca597cf082e2c17edd450f3ca59fcc3e5c
sirsasha <krypn87@mail.ru>
2025-12-17 22:50:01 +0800

    feat(sellers, shop): add models Seller, Category and Product

--- apps/sellers/models.py
+++ apps/sellers/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+from apps.accounts.models import User
+from apps.common.models import BaseModel
+
+
+class Seller(BaseModel):
+    """Модель продавца на маркетплейсе.
+    Описывает юридическое или физическое лицо, зарегистрированное как продавец.
+    Связана с пользователем через отношение один к одному. Содержит реквизиты
+    бизнеса, контактную информацию, банковские данные и статус модерации."""
+
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="seller",
+        verbose_name="Пользователь",
+    )
+    business_name = models.CharField(max_length=255, verbose_name="Название компании")
+    slug = AutoSlugField(
+        populate_from="business_name", always_update=True, null=True, verbose_name="URL"
+    )
+    inn_identification_number = models.CharField(max_length=50, verbose_name="ИНН")
+    website_url = models.URLField(null=True, verbose_name="Сайт")
+    phone_number = models.CharField(max_length=20, verbose_name="Телефон")
+    business_description = models.TextField(verbose_name="Описание бизнеса")
+    business_address = models.CharField(max_length=255, verbose_name="Адрес бизнеса")
+    city = models.CharField(max_length=100, verbose_name="Город")
+    postal_code = models.CharField(max_length=20, verbose_name="Почтовый индекс")
+    bank_name = models.CharField(max_length=255, verbose_name="Название банка")
+    bank_bic_number = models.CharField(max_length=9)
+    bank_account_number = models.CharField(
+        max_length=50, verbose_name="Банковский счет"
+    )
+    bank_routing_number = models.CharField(max_length=50)
+    is_approved = models.BooleanField(default=False, verbose_name="Проверен")
+
+    def __str__(self) -> str:
+        """Возвращает строковое представление объекта продавца."""
+
+        return f"Продавец из {self.business_name}"
--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from autoslug import AutoSlugField
 
-# Create your models here.
+from apps.common.models import BaseModel, IsDeletedModel
+from apps.sellers.models import Seller
+
+
+class Category(BaseModel):
+    """Модель категории для классификации товаров на маркетплейсе."""
+
+    name = models.CharField(max_length=100, unique=True, verbose_name="Название")
+    slug = AutoSlugField(
+        populate_from="name", unique=True, always_update=True, verbose_name="URL"
+    )
+    image = models.ImageField(upload_to="category_images/", verbose_name="Фото")
+
+    def __str__(self) -> str:
+        """Возвращает строковое представление категории."""
+
+        return self.name
+
+    class Meta:
+        """Мета-класс для настройки поведения модели."""
+
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+
+class Product(IsDeletedModel):
+    """Модель товара для платформы-маркетплейса.
+    Описывает продукт, выставленный на продажу продавцом. Включает в себя
+    основную информацию: название, описание, цены, количество на складе,
+    изображения и связь с продавцом. Поддерживает мягкое удаление — товар
+    можно скрыть без потери данных."""
+
+    seller = models.ForeignKey(
+        Seller,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="products",
+        verbose_name="Продавец",
+    )
+    name = models.CharField(max_length=100, verbose_name="Название")
+    slug = AutoSlugField(
+        populate_from="name", unique=True, db_index=True, verbose_name="URL"
+    )
+    description = models.TextField(verbose_name="Описание")
+    price_old = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, verbose_name="Старая цена"
+    )
+    price_current = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Текущая цена"
+    )
+    in_stock = models.IntegerField(default=5, verbose_name="Количество товара")
+    image1 = models.ImageField(
+        upload_to="product_images/", verbose_name="Первое изображение"
+    )
+    image2 = models.ImageField(
+        upload_to="product_images/", blank=True, verbose_name="Второе изображение"
+    )
+    image3 = models.ImageField(
+        upload_to="product_images/", blank=True, verbose_name="Третье изображение"
+    )
+
+    def __str__(self) -> str:
+        """Возвращает строковое представление товара."""
+
+        return self.name
+
+    class Meta:
+        """Мета-класс для настройки поведения модели."""
+
+        verbose_name = "Товар"
+        verbose_name_plural = "Товары"
